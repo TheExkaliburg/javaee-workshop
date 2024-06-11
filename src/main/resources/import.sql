4832262e09ba94f5e3c9b7667617b19b0d992672
Andreas Marx <andreas-johannes.marx@dedalus.eu>
2024-06-11 15:45:53 +0200

    test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,7 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+insert into ANIMAL
+    (uuid, name, type, comment, available, owner)
+values
+    ('5e64362e-b8e8-4361-ae99-b44109ba20a4', 'Miezi', 'WALRUS', NULL, 'true', NULL);
