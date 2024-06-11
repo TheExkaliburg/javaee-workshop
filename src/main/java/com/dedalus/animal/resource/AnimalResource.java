5be2f984e103b5be888cf144131834dd36ea6b8f
Tobias Schmidt <tobias.schmidt@dedalus.eu>
2024-06-11 15:46:13 +0200

    fix double endpoints

--- src/main/java/com/dedalus/animal/resource/AnimalResource.java
+++ src/main/java/com/dedalus/animal/resource/AnimalResource.java
@@ -1,32 +1,20 @@
 package com.dedalus.animal.resource;
 
-import java.util.List;
-import java.util.UUID;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
 import com.dedalus.animal.model.AnimalDetailedDto;
-import com.dedalus.animal.model.AnimalDto;
 import com.dedalus.animal.model.OwnerDto;
 import com.dedalus.animal.service.AnimalService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
 
 @Path("animal")
 public class AnimalResource {
 
     @Inject
     AnimalService animalService;
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<AnimalDto> listAll() {
-        return animalService.findAll();
-    }
 
     @Path("adopt")
     @POST
@@ -38,7 +26,11 @@ public class AnimalResource {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public AnimalDetailedDto find(@QueryParam(value = "uuid") UUID uuid) {
-        return animalService.findByUuid(uuid);
+    public Response find(@QueryParam(value = "uuid") UUID uuid) {
+        if (uuid == null) {
+            return Response.ok(animalService.findAll()).build();
+        }
+
+        return Response.ok(animalService.findByUuid(uuid)).build();
     }
 }
